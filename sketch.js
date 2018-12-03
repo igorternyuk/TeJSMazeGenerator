20aad06a05925ccdc8b8a092f28e1364e384fada
igorternyuk <mr.ternyuk@mail.ru>
2018-12-03 11:48:22 +0200

    creates the list of Cell objects

--- sketch.js
+++ sketch.js
@@ -1,19 +1,47 @@
-var canvasWidth = 400;
-var canvasHeight = 400;
+var CANVAS_WIDTH = 400;
+var CANVAS_HEIGHT = 400;
+const CELL_SIZE = 40;
+
+var cols, rows;
+var grid = [];
 
 function setup() {
-    createCanvas(canvasWidth, canvasHeight);
+    createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
     frameRate(10);
+    cols = CANVAS_WIDTH / CELL_SIZE;
+    rows = CANVAS_HEIGHT / CELL_SIZE;
+    for(let row = 0; row < rows; ++row){
+    	let line = [];
+    	for(let col = 0; col < cols; ++col){
+    		line.push(new Cell(row, col))
+    	}
+    	grid.push(line);
+    }
 }
 
 
 //main loop
 function draw() {
-	background(0);
+	background(70, 70, 70);
+	for(let row = 0; row < rows; ++row){
+		for(let col = 0; col < cols; ++col){
+			grid[row][col].render();
+		}
+	}
 }
 
-function Cell(i, j){
-	this.i = i;
-	this.j = j;
+class Cell {
+	constructor(row, col){
+		this.row = row;
+		this.col = col;
+		this.x = this.col * CELL_SIZE;
+		this.y = this.row * CELL_SIZE;	
+	}
+	
+	render(){
+		stroke(255);
+		noFill();
+		rect(this.x, this.y, CELL_SIZE, CELL_SIZE);
+	}
 }
 
